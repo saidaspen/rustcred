a5a9b898d98b83a72578e5295d5e9965ea247287
Said Aspen <mail@saidaspen.se>
2020-04-21 14:29:51 +0200

    pulls users and repos from github

--- src/github.rs
+++ src/github.rs
@@ -5,11 +5,27 @@ use serde::Deserialize;
 /// User representation of the GitHub API.
 /// Note that the GitHub API has several more fields, not all of them are interesting to us.
 /// Have a look at the GitHub API Documentation for details: https://developer.github.com/v3/users/
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct User {
-    login: String,
+    pub login: String,
     id: u32,
     url: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PrSearchResp {
+    total_count: u32,
+    items: Vec<PrSearchItem>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PrSearchItem {
+    url: String,
+    repository_url: String,
+    id: u32,
+    user: User,
+    state: String,
+    score: f64,
 }
 
 /// Get's all the GitHub users who have starred the RustCred repository
@@ -26,6 +42,41 @@ pub fn get_participants() -> Result<Vec<User>, Box<dyn error::Error>> {
 }
 
 /// Gets the number of merged pull-requests a certain author has for a certain GitHub Repository
-pub fn get_num_merged_prs(repo: &str, author: &str) -> u32 {
-    unimplemented!();
+/// TODO: Handle paging
+pub fn prs_for_user(author: &str) -> Result<Vec<PrSearchItem>, Box<dyn error::Error>> {
+    let query = format!(
+        "https://api.github.com/search/issues?q=author:{}+state:closed+is:pr",
+        author
+    );
+    let resp = reqwest::blocking::Client::new()
+        .get(&query)
+        // User-Agent is mandated by the GitHub API, if not supplied, request will be rejected.
+        .header("User-Agent", "RustCred App")
+        .header("Accept", "application/vnd.github.cloak-preview")
+        .send()?
+        .json::<PrSearchResp>()?;
+    Ok(resp.items)
+}
+
+pub fn lines_of(
+    repo: &str,
+    branch: &str,
+    file: &str,
+) -> Result<Vec<String>, Box<dyn error::Error>> {
+    let query = format!(
+        "https://raw.githubusercontent.com/{}/{}/{}",
+        repo, branch, file
+    );
+    let resp: String = reqwest::blocking::Client::new()
+        .get(&query)
+        // User-Agent is mandated by the GitHub API, if not supplied, request will be rejected.
+        .header("User-Agent", "RustCred App")
+        .header("Accept", "application/vnd.github.cloak-preview")
+        .send()?
+        .text()?;
+    Ok(resp
+        .lines()
+        .filter(|s| !s.trim().is_empty())
+        .map(|s| String::from(s))
+        .collect::<Vec<String>>())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
 mod github;
 
-
 fn main() {
-    let participants = match github::get_participants() {
+    // Get participants
+    let participants: Vec<github::User> = match github::get_participants() {
         Ok(users) => users,
-        _ => panic!("Unable to get participants"),
+        _ => panic!("Unable to get participants."),
     };
-    println!("{:?}", participants);
+    println!("All participants: {:?}", participants);
+
+    // Read the users who has opted out
+    let opted_out: Vec<String> =
+        github::lines_of("saidaspen/rustcred", "master", "opted_out").unwrap_or_else(|x| vec![]);
+    println!("Users who has opted out: {:?}", opted_out);
+
+    // Filter out participants who have opted out
+    let participants: Vec<github::User> = participants
+        .iter()
+        .filter(|p| !opted_out.contains(&p.login))
+        .map(|x| x.clone())
+        .collect();
+    println!("All participants (after filtering): {:?}", participants);
+
+    // Get all tracked repos
+    let tracked_repos: Vec<String> =
+        github::lines_of("saidaspen/rustcred", "master", "tracked_repos")
+            .expect("expected to find tracked_repos file");
+    println!("Tracked repos: {:?}", tracked_repos);
+
+    // for each participant {
+
+    //  Get all closed PRs for a specific user
+    //  let prs = github::prs_for_user("saidaspen");
+    //
+    //  Filter out those which are in relevant repos
+    //  Group them per repo and count
+    //  Map to Score per Repo
+    // }
+    //
+    // Generate HTML page for overview
+    // For each user {
+    //  Generate HTML for user
+    // }
 }
-
-
