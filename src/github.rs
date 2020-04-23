c9f9c75b45db9dbbe3869ca2d465dfdd8f6e07b1
Said Aspen <mail@saidaspen.se>
2020-04-23 12:43:36 +0200

    should create stats

--- src/github.rs
+++ src/github.rs
@@ -1,8 +1,9 @@
 use std::error;
 
 use serde::Deserialize;
-const STARGAZERS: &str = "/repos/saidaspen/rustcred/stargazers";
 const APP_NAME: &str = "RustCred";
+const REPO_URL: &str = "https://api.github.com/repos/";
+const USER_PER_PAGE: u32 = 100;
 
 /// User representation of the GitHub API.
 /// Note that the GitHub API has several more fields, not all of them are interesting to us.
@@ -17,15 +18,14 @@ pub struct User {
 #[derive(Deserialize, Debug)]
 pub struct PrSearchResp {
     total_count: u32,
-    items: Vec<PrSearchItem>,
+    items: Vec<Pr>,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct PrSearchItem {
-    url: String,
-    repository_url: String,
+#[derive(Deserialize, Debug, Clone)]
+pub struct Pr {
+    pub url: String,
+    pub repository_url: String,
     id: u32,
-    user: User,
     state: String,
     score: f64,
 }
@@ -40,79 +40,178 @@ fn github_url() -> String {
 
 fn raw_content_github_url() -> String {
     #[cfg(not(test))]
-    return String::from("raw.githubusercontent.com");
+    return String::from("https://raw.githubusercontent.com");
 
     #[cfg(test)]
     mockito::server_url()
 }
 
-/// Get's all the GitHub users who have starred the RustCred repository
-/// GitHub Users who have starred the repository is considered to be participants in RustCred.
-/// It is implemented by blocking, so it will block the outgoing web-request to GitHub and will
-/// thus take a while to execute.
-pub fn get_participants() -> Result<Vec<User>, Box<dyn error::Error>> {
-    Ok(reqwest::blocking::Client::new()
-        .get(&format!("{}{}", github_url(), STARGAZERS))
-        .header("User-Agent", APP_NAME)
-        .send()?
-        .json::<Vec<User>>()?)
+pub struct GitHubConn {
+    pub token: String,
+    pub repo: String,
 }
 
-/// Gets the number of merged pull-requests a certain author has for a certain GitHub Repository
-/// TODO: Handle paging
-pub fn prs_for_user(author: &str) -> Result<Vec<PrSearchItem>, Box<dyn error::Error>> {
-    Ok(reqwest::blocking::Client::new()
-        .get(&format!(
-            "{}/search/issues?q=author:{}+state:closed+is:pr",
+impl GitHubConn {
+    pub fn new(token: String, repo: String) -> GitHubConn {
+        GitHubConn {
+            token: token,
+            repo: repo,
+        }
+    }
+
+    fn query_gh(&self, url: &String) -> Result<String, Box<dyn error::Error>> {
+        Ok(reqwest::blocking::Client::new()
+            .get(url)
+            .header("User-Agent", APP_NAME)
+            .header("Authorization", &self.token)
+            .header("Accept", "application/vnd.github.cloak-preview")
+            .send()?
+            .text()?)
+    }
+
+    /// Get's all the GitHub users who have starred the RustCred repository
+    /// GitHub Users who have starred the repository is considered to be participants in RustCred.
+    /// It is implemented by blocking, so it will block the outgoing web-request to GitHub and will
+    /// thus take a while to execute.
+    pub fn get_participants(&self) -> Result<Vec<User>, Box<dyn error::Error>> {
+        let mut page: u32 = 0;
+        let mut users = vec![];
+        while users.len() % USER_PER_PAGE as usize == 0 {
+            page = page + 1;
+            let additional_users = &self.get_participants_page(page)?;
+            if additional_users.len() == 0 {
+                break;
+            }
+            users = [&users[..], &additional_users].concat();
+        }
+        Ok(users)
+    }
+
+    fn get_participants_page(&self, page: u32) -> Result<Vec<User>, Box<dyn error::Error>> {
+        let url = &format!(
+            "{}/repos/{}/stargazers?per_page={}&page={}",
             github_url(),
-            author
-        ))
-        .header("User-Agent", APP_NAME)
-        .header("Accept", "application/vnd.github.cloak-preview")
-        .send()?
-        .json::<PrSearchResp>()?
-        .items)
+            &self.repo,
+            USER_PER_PAGE,
+            page
+        );
+        Ok(serde_json::from_str(&self.query_gh(url)?)?)
+    }
+
+    /// Gets the number of merged pull-requests a certain author has for a certain GitHub Repository
+    pub fn merged_prs_for(&self, author: &str) -> Result<Vec<Pr>, Box<dyn error::Error>> {
+        let mut page: u32 = 0;
+        let mut prs = vec![];
+        while prs.len() % USER_PER_PAGE as usize == 0 {
+            page = page + 1;
+            let additional_prs = &self.get_merged_prs_page(&author, page)?;
+            if additional_prs.len() == 0 {
+                break;
+            }
+            prs = [&prs[..], &additional_prs].concat();
+        }
+        Ok(prs)
+    }
+
+    fn get_merged_prs_page(
+        &self,
+        author: &str,
+        page: u32,
+    ) -> Result<Vec<Pr>, Box<dyn error::Error>> {
+        let url = format!(
+            "{}/search/issues?q=author:{}+state:closed+is:merged&per_page={}&page={}",
+            github_url(),
+            author,
+            USER_PER_PAGE,
+            page
+        );
+        let prs: PrSearchResp = serde_json::from_str(&self.query_gh(&url)?.to_string())?;
+        Ok(prs.items)
+    }
+
+    pub fn lines_of(&self, branch: &str, file: &str) -> Result<Vec<String>, Box<dyn error::Error>> {
+        let url = format!(
+            "{}/{}/{}/{}",
+            raw_content_github_url(),
+            &self.repo,
+            branch,
+            file
+        );
+        Ok(self
+            .query_gh(&url)?
+            .lines()
+            .filter(|s| !s.trim().is_empty())
+            .map(|s| String::from(s.trim()))
+            .collect::<Vec<String>>())
+    }
 }
 
-pub fn lines_of(
-    repo: &str,
-    branch: &str,
-    file: &str,
-) -> Result<Vec<String>, Box<dyn error::Error>> {
-    let url = format!("{}/{}/{}/{}", raw_content_github_url(), repo, branch, file);
-    println!("{:?}", url);
-
-    Ok(reqwest::blocking::Client::new()
-        .get(&url)
-        .header("User-Agent", APP_NAME)
-        .header("Accept", "application/vnd.github.cloak-preview")
-        .send()?
-        .text()?
-        .lines()
-        .filter(|s| !s.trim().is_empty())
-        .map(|s| String::from(s.trim()))
-        .collect::<Vec<String>>())
+pub fn repo_name(url: &str) -> String {
+    url[REPO_URL.len()..].to_string()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::github::{get_participants, lines_of, User, STARGAZERS};
+    use crate::github::{repo_name, GitHubConn, User};
     use mockito::mock;
+    use std::error;
+
+    #[test]
+    fn test_repo_name() {
+        assert_eq!(
+            repo_name("https://api.github.com/repos/saidaspen/rustcred"),
+            "saidaspen/rustcred"
+        );
+    }
+
+    fn mock_with(url: &str, body: &str) -> mockito::Mock {
+        mock("GET", url)
+            .with_status(200)
+            .with_header("content-type", "application/json; charset=utf-8")
+            .with_body(body)
+            .create()
+    }
+
+    #[test]
+    fn gets_pr_for_user_w_paging() -> Result<(), Box<dyn error::Error>> {
+        let conn = GitHubConn::new("test".to_string(), "saidaspen/rustcred".to_string());
+        let pr = "{\"url\": \"someurl\", \"repository_url\": \"repo_url\", \"id\": 123, \"state\": \"closed\", \"score\":1.0}";
+        let prs_first = vec![pr; 100];
+        let first_page = format!(
+            "{{\"total_count\":{}, \"items\":[{}]}}",
+            prs_first.len(),
+            prs_first.join(", ")
+        );
+        let prs_second = vec![pr; 10];
+        let second_page = format!(
+            "{{\"total_count\":{}, \"items\":[{}]}}",
+            prs_second.len(),
+            prs_second.join(", ")
+        );
+        let _m = mock_with(
+            "/search/issues?q=author:saidaspen+state:closed+is:merged&per_page=100&page=1",
+            &first_page,
+        );
+        let _m = mock_with(
+            "/search/issues?q=author:saidaspen+state:closed+is:merged&per_page=100&page=2",
+            &second_page,
+        );
+        let prs = conn.merged_prs_for("saidaspen")?;
+        assert_eq!(prs.len(), 110);
+        Ok(())
+    }
 
     #[test]
     fn gets_lines_of_file() {
+        let conn = GitHubConn::new("test".to_string(), "saidaspen/rustcred".to_string());
         // Here we also have a newline in the middle to test it does not add those.
         let body = r#"
         line1 
 
         line2 
         "#;
-        let _m = mock("GET", "/saidaspen/rustcred/master/somefile")
-            .with_status(200)
-            .with_header("content-type", "text/plain; charset=utf-8")
-            .with_body(&body)
-            .create();
-        let opted_out_users = match lines_of("saidaspen/rustcred", "master", "somefile") {
+        let _m = mock_with("/saidaspen/rustcred/master/somefile", &body);
+        let opted_out_users = match conn.lines_of("master", "somefile") {
             Ok(l) => l,
             Err(e) => panic!("unable to get opted out users: {:?}", e),
         };
@@ -122,46 +221,89 @@ mod tests {
     }
 
     #[test]
-    fn gets_participants() {
-        let body = r#"
-        [
-            {
-                "login": "firstuser",
-                "id": 1,
-                "url": "https://api.github.com/users/firstuser"
-            },
-            {
-                "login": "user_w_unused_field",
-                "id": 2,
-                "url": "https://api.github.com/users/user_w_unused_field",
-                "some_unused_fields": "value"
-            }
-        ]"#;
-        let _m = mock("GET", STARGAZERS)
-            .with_status(200)
-            .with_header("content-type", "application/json; charset=utf-8")
-            .with_body(&body)
-            .create();
-        let participants = match get_participants() {
-            Ok(p) => p,
-            Err(e) => panic!("unable to get participants {:?}", e),
-        };
-        assert_eq!(participants.len(), 2);
+    fn get_participants_paging_second_empty() -> Result<(), Box<dyn error::Error>> {
+        let conn = GitHubConn::new("test".to_string(), "saidaspen/rustcred".to_string());
+        let first_page = format!(
+            "[{}]\n",
+            vec!["{\"login\": \"user\", \"id\": 1, \"url\": \"https://someuserurl\"}"; 100]
+                .join(", ")
+        );
+        let _m = mock_with(
+            "/repos/saidaspen/rustcred/stargazers?per_page=100&page=1",
+            &first_page,
+        );
+        let _m = mock_with(
+            "/repos/saidaspen/rustcred/stargazers?per_page=100&page=2",
+            "[]",
+        );
+        assert_eq!(conn.get_participants()?.len(), 100);
+        Ok(())
+    }
+
+    #[test]
+    fn get_participants_paging() -> Result<(), Box<dyn error::Error>> {
+        let conn = GitHubConn::new("test".to_string(), "saidaspen/rustcred".to_string());
+        let mut page: Vec<&str> =
+            vec!["{\"login\": \"user\", \"id\": 1, \"url\": \"https://someuserurl\"}"; 99];
+        let other_user = "{\"login\": \"other_user\", \"id\": 2, \"url\": \"https://someuserurl\"}";
+        page.push(other_user);
+        let first_page = format!("[{}]\n", &page.join(", "));
+        let second_page = format!("[{}]\n", &page.join(", "));
+        let third_page = format!("[{}]\n", vec![other_user, other_user].join(", "));
+        let _m = mock_with(
+            "/repos/saidaspen/rustcred/stargazers?per_page=100&page=1",
+            &first_page,
+        );
+        let _m = mock_with(
+            "/repos/saidaspen/rustcred/stargazers?per_page=100&page=2",
+            &second_page,
+        );
+        let _m = mock_with(
+            "/repos/saidaspen/rustcred/stargazers?per_page=100&page=3",
+            &third_page,
+        );
+        let participants = conn.get_participants()?;
+        assert_eq!(participants.len(), 202);
         assert_eq!(
             participants[0],
             User {
-                login: "firstuser".to_string(),
+                login: "user".to_string(),
                 id: 1,
-                url: "https://api.github.com/users/firstuser".to_string()
+                url: "https://someuserurl".to_string(),
             }
         );
         assert_eq!(
-            participants[1],
+            participants[99],
             User {
-                login: "user_w_unused_field".to_string(),
+                login: "other_user".to_string(),
                 id: 2,
-                url: "https://api.github.com/users/user_w_unused_field".to_string()
+                url: "https://someuserurl".to_string(),
             }
         );
+        assert_eq!(
+            participants[201],
+            User {
+                login: "other_user".to_string(),
+                id: 2,
+                url: "https://someuserurl".to_string(),
+            }
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn gets_participants_no_users() -> Result<(), Box<dyn error::Error>> {
+        let conn = GitHubConn::new("test".to_string(), "saidaspen/rustcred".to_string());
+        let _m = mock(
+            "GET",
+            "/repos/saidaspen/rustcred/stargazers?per_page=100&page=1",
+        )
+        .with_status(200)
+        .with_header("content-type", "application/json; charset=utf-8")
+        .with_body("[]")
+        .create();
+        let participants = conn.get_participants()?;
+        assert_eq!(participants.len(), 0);
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 mod github;
-use github::{get_participants, lines_of, User};
+use github::{repo_name, GitHubConn, User};
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::env;
 
-const REPO: &str = "saidaspen/rustcred";
+const REPO: &str = "paupino/rust-decimal"; //"saidaspen/rustcred";
 const BRANCH: &str = "master";
+const TOKEN_PROP_NAME: &str = "RC_GITHUB_TOKEN";
 
 fn main() {
+    let github_token = match env::var(TOKEN_PROP_NAME) {
+        Ok(s) => s,
+        Err(_) => {
+            println!(
+                r#"Environment property {} not set.
+This needs to be set to your personal access token from GitHub.
+See https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line"#,
+                TOKEN_PROP_NAME
+            );
+            return;
+        }
+    };
+
+    let gh = GitHubConn::new(github_token, REPO.to_string());
+
     // Get participants
-    let participants: Vec<User> = get_participants().expect("Unable to get partricipants");
-    println!("All participants: {:?}", participants);
+    let participants: Vec<User> = gh.get_participants().expect("Unable to get partricipants.");
 
     // Read the users who has opted out
-    let opted_out: Vec<String> = lines_of(REPO, BRANCH, "opted_out").unwrap_or_else(|_| vec![]);
-    println!("Users who has opted out: {:?}", opted_out);
+    let opted_out: Vec<String> = gh.lines_of(BRANCH, "opted_out").unwrap_or_else(|_| vec![]);
 
     // Filter out participants who have opted out
     let participants: Vec<User> = participants
@@ -19,25 +36,48 @@ fn main() {
         .filter(|p| !opted_out.contains(&p.login))
         .map(|x| x.clone())
         .collect();
-    println!("All participants (after filtering): {:?}", participants);
+
+    print_participants(&participants);
+    println!("{}", &participants.len());
 
     // Get all tracked repos
-    let tracked_repos: Vec<String> =
-        lines_of(REPO, BRANCH, "tracked_repos").expect("expected to find tracked_repos file");
-    println!("Tracked repos: {:?}", tracked_repos);
+    let tracked_repos: HashSet<String> = gh
+        .lines_of(BRANCH, "tracked_repos")
+        .expect("expected to find tracked_repos file")
+        .iter()
+        .map(|repo| format!("https://api.github.com/repos/{}", repo))
+        .collect();
 
-    // for each participant {
-
-    //  Get all closed PRs for a specific user
-    //  let prs = github::prs_for_user("saidaspen");
-    //
-    //  Filter out those which are in relevant repos
-    //  Group them per repo and count
-    //  Map to Score per Repo
-    // }
+    for p in participants {
+        let prs = gh
+            .merged_prs_for(&p.login)
+            .unwrap_or_else(|e| panic!("unable to get PRs for user {}, error: {} ", p.login, e))
+            .into_iter()
+            .map(|pr| pr.repository_url)
+            .filter(|repo| tracked_repos.contains(repo))
+            .fold(HashMap::new(), |mut map, repo| {
+                *map.entry(repo_name(&repo)).or_insert(0) += 1;
+                map
+            });
+        println!("User: {}", &p.login);
+        println!("--------------------------------");
+        for (k, v) in prs.iter() {
+            println!("{}\t{}", k, v);
+        }
+    }
     //
     // Generate HTML page for overview
     // For each user {
     //  Generate HTML for user
     // }
+}
+
+fn print_participants(participants: &Vec<User>) {
+    println!(
+        "All participants (after filtering): {:?}",
+        participants
+            .iter()
+            .map(|p| p.login.to_string())
+            .collect::<Vec<String>>()
+    );
 }
